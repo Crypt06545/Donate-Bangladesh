bc06886dec22a63631dda6f7ce140c7794a523f6
Crypt06545 <mehedihasansagor2005@gmail.com>
2024-09-24 08:00:42 +0600

    fix blog and home page button

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 /// Selectors
 const dNoakhali = document.getElementById("noakhali-dontate-btn");
 const noakhaliInput = document.getElementById("noakhali-donation-input");
-const accountBalanceElement = document.getElementById("account-banalce");
-const noakhaliAmountElement = document.getElementById("noakhali-amount");
+const accountBalanceEl = document.getElementById("account-banalce");
+const noakhaliAmountEl = document.getElementById("noakhali-amount");
 
 const dFeni = document.getElementById("feni-dontate-btn");
 const feniInput = document.getElementById("feni-donation-input");
-const feniAmountElement = document.getElementById("feni-amount");
+const feniAmountEl = document.getElementById("feni-amount");
 
 const dQuota = document.getElementById("quota-dontate-btn");
 const quotaInput = document.getElementById("quota-donation-input");
-const quotaAmountElement = document.getElementById("quota-amount");
+const quotaAmountEl = document.getElementById("quota-amount");
 
 const donationHistorySection = document.getElementById("donation-history");
 const historyBtn = document.getElementById("history-btn");
@@ -21,8 +21,8 @@ const donateBtnTab = document.getElementById("donate-btn-tab");
 dNoakhali.addEventListener("click", function () {
   donation(
     noakhaliInput,
-    accountBalanceElement,
-    noakhaliAmountElement,
+    accountBalanceEl,
+    noakhaliAmountEl,
     "Flood at Noakhali"
   );
 });
@@ -30,8 +30,8 @@ dNoakhali.addEventListener("click", function () {
 dFeni.addEventListener("click", function () {
   donation(
     feniInput,
-    accountBalanceElement,
-    feniAmountElement,
+    accountBalanceEl,
+    feniAmountEl,
     "Flood Relief in Feni"
   );
 });
@@ -39,17 +39,17 @@ dFeni.addEventListener("click", function () {
 dQuota.addEventListener("click", function () {
   donation(
     quotaInput,
-    accountBalanceElement,
-    quotaAmountElement,
+    accountBalanceEl,
+    quotaAmountEl,
     "Aid for Injured in the Quota Movement"
   );
 });
 
 // Donation function
-function donation(inputElement, balanceElement, amountElement, location) {
-  const donationAmount = parseFloat(inputElement.value);
-  let balance = parseFloat(balanceElement.innerText);
-  let currentAmount = parseFloat(amountElement.innerText);
+function donation(inputEl, balanceEl, amountEl, location) {
+  const donationAmount = parseFloat(inputEl.value);
+  let balance = parseFloat(balanceEl.innerText);
+  let currentAmount = parseFloat(amountEl.innerText);
 
   if (
     !isNaN(donationAmount) &&
@@ -58,16 +58,16 @@ function donation(inputElement, balanceElement, amountElement, location) {
   ) {
     // Update balance
     balance -= donationAmount;
-    balanceElement.innerText = balance.toFixed(2);
+    balanceEl.innerText = balance.toFixed(2);
 
     currentAmount += donationAmount;
-    amountElement.innerText = currentAmount.toFixed(2);
+    amountEl.innerText = currentAmount.toFixed(2);
 
     const modal = document.getElementById("my_modal_5");
     modal.showModal();
 
     // Clear the input field
-    inputElement.value = "";
+    inputEl.value = "";
 
     // Add the donation to history
     donationHistory(donationAmount, location);
